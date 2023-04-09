bedde8c08bc524b2f48ae449d18aa70e897de692
Makarius Salib <misalib@cpp.edu>
2023-04-09 12:58:27 -0700

    Reads in the RGB data from the dataset
    - Assumes a folder structure
    - Creates a list of inputs
    - Creates a corresponding list of outputs

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#TODO: ALL OF IT
+from PIL import Image
+from os import listdir
+
+# Folder structure has a containing folder for cnn and the git repo
+pathToPics = "..\\cnn\\"
+
+X = [] # Inputs
+Y = [] # Ground truths
+
+for image in listdir(pathToPics + "Edible"):
+    im = Image.open(pathToPics + "Edible\\" + image, 'r')
+    X.append([x for sets in list(im.getdata()) for x in sets]) # Will add the list of RGB values
+    Y.append(1)
+    im.close()
+
+for image in listdir(pathToPics + "Poisonous"):
+    im = Image.open(pathToPics + "Poisonous\\" + image, 'r')
+    X.append([x for sets in list(im.getdata()) for x in sets])
+    Y.append(0)
+    im.close()
