04aa43a233256297be1fc51618009e62a6c7b570
Makarius Salib <misalib@cpp.edu>
2023-04-24 19:04:33 -0700

    Corrects the validation data - Validation data changes to the validation subset instead of the training set

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def main():
     data_dir = pathlib.Path(archive).with_suffix('')
 
     train_ds = tf.keras.utils.image_dataset_from_directory(data_dir, validation_split=.2, subset="training", seed=123, image_size=(img_height, img_width), batch_size=batch_size)
-    val_ds = tf.keras.utils.image_dataset_from_directory(data_dir, validation_split=.2, subset="training", seed=123, image_size=(img_height, img_width), batch_size=batch_size)
+    val_ds = tf.keras.utils.image_dataset_from_directory(data_dir, validation_split=.2, subset="validation", seed=123, image_size=(img_height, img_width), batch_size=batch_size)
     
     AUTOTUNE = tf.data.AUTOTUNE
     train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
